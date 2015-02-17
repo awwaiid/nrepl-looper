26ebb8aba9ff5ebf15ae9864fa8a7be923ff8dd0
Brock Wilcox <awwaiid@thelackthereof.org>
2015-02-17 14:56:03 -0500

    fix whitespace

--- src/looper/core.clj
+++ src/looper/core.clj
@@ -102,16 +102,16 @@
 
 ;;; From jammer!
 
-(def alphabet-keycodes (zipmap 
-                         ; (range 65 91) 
-                         (range 97 123) 
+(def alphabet-keycodes (zipmap
+                         ; (range 65 91)
+                         (range 97 123)
                          "abcdefghijklmnopqrstuvwxyz"))
 
 (def numeric-keycodes {49 \1
-                       50 \2 
+                       50 \2
                        51 \3
                        52 \4
-                       53 \5 
+                       53 \5
                        54 \6
                        55 \7
                        56 \8
@@ -121,13 +121,13 @@
                        46 \.
                        })
 
-(defn keycode->char  
+(defn keycode->char
   "'leaky' keycode->char converter
   gives 65 -> 'a'
   ie when given a code not a char, it just send the keycode through"
   [keycode]
-  (get 
-    (merge alphabet-keycodes numeric-keycodes)     
+  (get
+    (merge alphabet-keycodes numeric-keycodes)
     keycode keycode))
 
 (def char->note "
@@ -146,18 +146,18 @@
                 *   C    *  -> +2
                 *        *
                 *      *
-                ******       
+                ******
                 "
   {\1 64 \2 66 \3 68 \4 70 \5 72 \6 74 \7 76 \8 78 \9 80 \0 82
    \q 59 \w 61 \e 63 \r 65 \t 67 \y 69 \u 71 \i 73 \o 75 \p 77
-   \a 54 \s 56 \d 58 \f 60 \g 62 \h 64 \j 66 \k 68 \l 70 
+   \a 54 \s 56 \d 58 \f 60 \g 62 \h 64 \j 66 \k 68 \l 70
    60 47 \z 49 \x 51 \c 53 \v 55 \b 57 \n 59 \m 61 \, 63 \. 65} )
 
-(defn keycode->midinote 
-  [keycode] 
+(defn keycode->midinote
+  [keycode]
   {:test (fn [] ( = (keycode->midinote 65 ) 54))}
   "gets a keycode and output a suitable MIDI-note"
-  (-> keycode 
+  (-> keycode
       keycode->char
       char->note))
 
@@ -174,7 +174,7 @@
       (midi-connected-receivers))))
 
 (defn jam []
-  (while true 
+  (while true
     (let [note (- (keycode->midinote (get-keystroke)) 20)]
       (sampled-piano note)
       (midi-note midi-output-device note 127 500)
