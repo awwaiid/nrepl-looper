223957947baccb4076d32400197e953e365ab4c8
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-02 15:03:07 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nrepl-looper "0.1.0-SNAPSHOT"
+(defproject nrepl-looper "0.1.0"
   :description "nREPL middleware to replay command with timing, like a music looper"
   :url "https://github.com/awwaiid/nrepl-looper"
   :dependencies [[org.clojure/clojure "1.6.0"]
